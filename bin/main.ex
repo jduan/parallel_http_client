2c05005c2af7ebace62549e66922dc3734f0dd48
Jingjing Duan <jduan@fitbit.com>
2017-01-09 13:25:50 -0800

    max_connections not working yet

--- bin/main.ex
+++ bin/main.ex
@@ -1,14 +1,15 @@
-:ok = :hackney_pool.start_pool(:first_pool, [timeout: 15000, max_connections: 1000])
 HTTPoison.start
+:ok = :hackney_pool.start_pool(:first_pool, [timeout: 15000, max_connections: 100])
 
 # Calculate squares of these numbers
 parallelism = 100_000
-num_of_entries = 1_000_000
+num_of_entries = 1_000_000 - 1
 start_time = :os.system_time(:millisecond)
 results = Parallel.pmap(1..parallelism, fn(integer) ->
   randi = :rand.uniform(num_of_entries)
   # IO.puts "request #{randi}"
-  response = HTTPoison.get!("http://localhost:8888/hello/#{randi}", [], [recv_timeout: 1000000])
+  response = HTTPoison.get!("http://localhost:8888/hello/#{randi}", [],
+    [recv_timeout: 1000000, hackney: [pool: :first_pool]])
   # response = HTTPoison.get!("http://localhost:8888/hello?name=#{randi}", [], [recv_timeout: 1000000])
   # response = HTTPoison.get!("http://localhost:8888/hi?name=#{randi}", [], [recv_timeout: 1000000])
   # response = HTTPoison.get!("http://www.google.com", [], [recv_timeout: 1000000])
